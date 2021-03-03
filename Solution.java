fcf566148b26842a2f5ddbea958bd3df576fbd49
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-03-02 18:19:33 -0600

    Main File
    I am committing the main file that the application is comprised of (Java File).

---
+++ Solution.java
@@ -0,0 +1,15 @@
+/* 
+One cat isn't enough
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat kitty = new Cat();
+        Cat smokey = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
